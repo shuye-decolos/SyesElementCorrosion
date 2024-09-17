dbc04c109f5cbf346aec4b709b378336293d0c9a
shuye-decolos <2918723619@qq.com>
2024-09-17 11:25:34 +0800

    InitVersion

---
+++ src/main/java/com/shuye/elementcorrosion/biomes/ElementColorRegister.java
@@ -0,0 +1,32 @@
+package com.shuye.elementcorrosion.biomes;
+
+import com.shuye.elementcorrosion.blocks.RegisterBlock;
+import com.shuye.elementcorrosion.blocks.SyesECBlocks;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.color.IBlockColor;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.biome.BiomeColorHelper;
+import net.minecraftforge.client.event.ColorHandlerEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+
+@Mod.EventBusSubscriber
+public class ElementColorRegister {
+    @SubscribeEvent
+    @SideOnly(Side.CLIENT)
+    public static void registerElementColor(ColorHandlerEvent.Block event){
+        event.getBlockColors().registerBlockColorHandler(new IBlockColor() {
+            @Override
+            public int colorMultiplier(IBlockState state, @Nullable IBlockAccess worldIn, @Nullable BlockPos pos, int tintIndex) {
+                return worldIn != null && pos != null ? BiomeColorHelper.getGrassColorAtPos(worldIn, pos) : -1;
+            }
+        }, SyesECBlocks.BIOME_GLASS, SyesECBlocks.BIOME_GLASS_IGNIS
+        );
+    }
+}
